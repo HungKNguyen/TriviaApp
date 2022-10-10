fa244ff1d1af827502080551cb96f629ae6d6870
HungKNguyen <75971367+HungKNguyen@users.noreply.github.com>
2022-10-10 12:02:54 -0500

    Initial Commit

---
+++ TriviaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TriviaApp
+//
+//  Created by Hung K Nguyen on 10/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
